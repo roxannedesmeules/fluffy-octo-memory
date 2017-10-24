270a0a0933c4a096b194441574a4b7da38eaaa47
Roxanne Desmeules <roxanne@smartreno.com>
2017-10-24 08:01:39 -0400

    API - add cors filter to ControllerAdminEx

--- api/modules/v1/admin/components/ControllerAdminEx.php
+++ api/modules/v1/admin/components/ControllerAdminEx.php
@@ -2,10 +2,14 @@
 namespace app\modules\v1\admin\components;
 
 use app\helpers\ArrayHelperEx;
+use app\helpers\ParamsHelper;
 use app\modules\v1\admin\components\security\ApiClientSecurity;
 use app\modules\v1\admin\components\security\ApiTokenSecurity;
 use yii\filters\auth\HttpBasicAuth;
 use yii\rest\Controller;
+use yii\rest\OptionsAction;
+use yii\web\Request;
+use yii\web\Response;
 
 /**
  * Class ControllerEx
@@ -14,8 +18,14 @@ use yii\rest\Controller;
  */
 class ControllerAdminEx extends Controller
 {
+	/** @var  Request */
 	public $request;
+
+	/** @var  Response */
 	public $response;
+
+	/** @var array  */
+	public $corsMethods = [ "OPTIONS", "GET", "POST", "PUT", "DELETE" ];
 	
 	public function init ()
 	{
@@ -28,10 +38,27 @@ class ControllerAdminEx extends Controller
 	public function behaviors ()
 	{
 		return ArrayHelperEx::merge(parent::behaviors(), [
+			"corsFilter"    => [
+				"class" => \yii\filters\Cors::className(),
+				"cors"  => [
+					"Origin"                         => ParamsHelper::get("cors.origin"),
+					"Access-Control-Allow-Origin"    => ParamsHelper::get("cors.origin"),
+					"Access-Control-Request-Method"  => $this->corsMethods,
+					"Access-Control-Max-Age"         => 3600,
+					"Access-Control-Request-Headers" => [ "*" ],
+					'Access-Control-Allow-Credentials' => true,
+				],
+			],
 			"ClientToken"   => ApiClientSecurity::className(),
-			"Authenticator" => HttpBasicAuth::className(),
+			"Authenticator" => [
+				"class" => HttpBasicAuth::className(),
+				"except" => [ "options" ],
+			]
 		]);
 	}
-	
-	public function actionOptions () { }
+
+	public function actions ()
+	{
+		return ArrayHelperEx::merge(parent::actions(), [ "options" => OptionsAction::className(), ]);
+	}
 }
