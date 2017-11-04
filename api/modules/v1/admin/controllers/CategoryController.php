301469c9d2767106380c4a85b28752b971c123e1
Roxanne Desmeules <roxanne@smartreno.com>
2017-11-03 23:03:15 -0400

    updated the category controller to return errors correctly when creation fails  updated response format

--- api/modules/v1/admin/controllers/CategoryController.php
+++ api/modules/v1/admin/controllers/CategoryController.php
@@ -3,8 +3,10 @@ namespace app\modules\v1\admin\controllers;
 
 use app\modules\v1\admin\components\ControllerAdminEx;
 use app\modules\v1\admin\models\category\CategoryEx;
+use app\modules\v1\admin\models\category\CategoryLangEx;
 use yii\data\ArrayDataProvider;
-use yii\web\BadRequestHttpException;
+use yii\web\ServerErrorHttpException;
+use yii\web\UnprocessableEntityHttpException;
 
 /**
  * Class CategoryController
@@ -31,9 +33,35 @@ class CategoryController extends ControllerAdminEx
 	{
 		//  get request data and create a Category with it
 		$form = new CategoryEx($this->request->getBodyParams());
+
+		if ( !$form->validate() ) {
+			return $this->unprocessableResult($form->getErrors());
+		}
 		
 		//  create the category with translation and return result
-		return CategoryEx::createWithTranslations($form, $form->translations);
+		$result = CategoryEx::createWithTranslations($form, $form->translations);
+
+		//  if the status is an error, then define what to do depending on the content
+		if ($result[ "status" ] === CategoryEx::ERROR) {
+			switch ($result[ "error" ]) {
+				case CategoryEx::ERR_ON_SAVE :
+					throw new ServerErrorHttpException(CategoryEx::ERR_ON_SAVE);
+
+				case CategoryLangEx::ERR_LANG_NOT_FOUND :
+					throw new UnprocessableEntityHttpException(CategoryLangEx::ERR_LANG_NOT_FOUND);
+
+				default :
+					if ( is_array($result[ "error" ]) ) {
+						$this->unprocessableResult($result[ "error" ]);
+					}
+			}
+		}
+
+
+		//  return newly created category ID
+		$this->response->setStatusCode(201);
+
+		return [ "category_id" => $result[ "category_id" ] ];
 	}
 	
 	public function actionUpdate ( $id )
