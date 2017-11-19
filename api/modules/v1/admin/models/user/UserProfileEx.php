d01fdb3afad9823545fa2c8530ea99a83c455b36
Roxanne Desmeules <roxanne@smartreno.com>
2017-11-19 14:47:14 -0500

    Created Password form to check the properties required in the update password request  Updated the UserProfileEx model to added rules

--- api/modules/v1/admin/models/user/UserProfileEx.php
+++ api/modules/v1/admin/models/user/UserProfileEx.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\modules\v1\admin\models\user;
 
 use app\models\user\UserProfile;
@@ -8,20 +7,23 @@ use app\models\user\UserProfile;
  * Class UserProfileEx
  *
  * @package app\modules\v1\admin\models\user
- *
- * @SWG\Definition(
- *     definition = "UserProfile",
- *
- *     @SWG\Property( property = "firstname", type = "string" ),
- *     @SWG\Property( property = "lastname", type = "string" ),
- *     @SWG\Property( property = "fullname", type = "string" ),
- *     @SWG\Property( property = "birthday", type = "string" ),
- * )
  */
 class UserProfileEx extends UserProfile
 {
 	const DATE_FORMAT = "Y-m-d";
-	
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @SWG\Definition(
+	 *     definition = "UserProfile",
+	 *
+	 *     @SWG\Property( property = "firstname", type = "string" ),
+	 *     @SWG\Property( property = "lastname", type = "string" ),
+	 *     @SWG\Property( property = "fullname", type = "string" ),
+	 *     @SWG\Property( property = "birthday", type = "string" ),
+	 * )
+	 */
 	public function fields ()
 	{
 		return [
@@ -29,6 +31,26 @@ class UserProfileEx extends UserProfile
 			"lastname",
 			"fullname" => function ( self $model ) { return "$model->firstname $model->lastname"; },
 			"birthday" => function ( self $model ) { return date(self::DATE_FORMAT, strtotime($model->birthday)); },
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @SWG\Definition(
+	 *     definition = "UserProfileForm",
+	 *
+	 *     @SWG\Property( property = "firstname", type = "string", minLength = 2 ),
+	 *     @SWG\Property( property = "lastname", type = "string", minLength = 2 ),
+	 *     @SWG\Property( property = "birthday", type = "string", format = "date" ),
+	 * )
+	 */
+	public function rules ()
+	{
+		return [
+			[ "firstname", "string" ],
+			[ "lastname", "string" ],
+			[ "birthday", "string" ],
 		];
 	}
 }
