e28412d9f5ec23a6a2a68f6ffe0181917aed5a8e
Roxanne Desmeules <roxanne@smartreno.com>
2017-12-10 22:20:53 -0500

    updated test configurations  removed functional tests since unnecessary  updated unit tests config

--- api/config/test_db.php
+++ api/config/test_db.php
@@ -2,7 +2,7 @@
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=blog_test';
-$db['username'] = 'root';
-$db['password'] = '123';
+$db['username'] = 'blog_user';
+$db['password'] = '1234';
 
 return $db;
