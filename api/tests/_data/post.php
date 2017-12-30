88ea62e68fea2ce0fd83a7b38a5f65ae8764c7d4
Roxanne Desmeules <roxanne@smartreno.com>
2017-12-29 20:53:25 -0500

    reviewed index of post and post lang data

--- api/tests/_data/post.php
+++ api/tests/_data/post.php
@@ -3,8 +3,8 @@
 $faker = \Faker\Factory::create();
 
 return [
-	"post1" => [ "category_id" => 1, "post_status_id" => 1, ],
-	"post2" => [ "category_id" => 1, "post_status_id" => 1, ],
-	"post3" => [ "category_id" => 2, "post_status_id" => 3, ],
-	"post4" => [ "category_id" => 2, "post_status_id" => 2, ],
+	"draft"       => [ "category_id" => 1, "post_status_id" => 1, ],
+	"archived"    => [ "category_id" => 1, "post_status_id" => 4, ],
+	"published"   => [ "category_id" => 2, "post_status_id" => 2, ],
+	"unpublished" => [ "category_id" => 2, "post_status_id" => 3, ],
 ];
