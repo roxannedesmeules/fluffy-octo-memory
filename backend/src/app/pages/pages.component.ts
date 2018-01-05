640f33b26382fc1b1c7408ea0e1ed8c4a020b054
Roxanne Desmeules <roxanne@smartreno.com>
2018-01-05 16:37:59 -0500

    remove theme switcher and set theme as default (not cosmic)

--- backend/src/app/pages/pages.component.ts
+++ backend/src/app/pages/pages.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { MENU_ITEMS } from './pages-menu';
+import { MENU_ITEMS } from "./pages-menu";
 
 @Component({
-  selector: 'ngx-pages',
-  template: `
-    <ngx-sample-layout>
-      <nb-menu [items]="menu"></nb-menu>
-      <router-outlet></router-outlet>
-    </ngx-sample-layout>
-  `,
+	selector : "ngx-pages",
+	template : `
+		<ngx-sample-layout>
+			<nb-menu [items]="menu"></nb-menu>
+			<router-outlet></router-outlet>
+		</ngx-sample-layout>
+	`,
 })
 export class PagesComponent {
 
-  menu = MENU_ITEMS;
+	menu = MENU_ITEMS;
 }
