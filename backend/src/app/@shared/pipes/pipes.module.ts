2177ed83e3629b99884fecb1016958963bc38769
Roxanne Desmeules <roxanne@smartreno.com>
2018-01-09 22:01:09 -0500

    Created ColumnPipe and VerbalBooleanPipe  Updated categories list :   - show all languages for a category   - transform a true/false or 1/0 into a Yes/No

--- backend/src/app/@shared/pipes/pipes.module.ts
+++ backend/src/app/@shared/pipes/pipes.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { AtIndexOfPipe } from "./at-index-of.pipe";
+import { ColumnPipe } from "./column.pipe";
 import { SlugPipe } from "./slug.pipe";
+import { VerbalBooleanPipe } from "./verbal-boolean.pipe";
 
 const SHARED_PIPES = [
 	AtIndexOfPipe,
+	ColumnPipe,
 	SlugPipe,
+	VerbalBooleanPipe,
 ];
 
 @NgModule({
