75305bbc2482d5075a4c6a0affaa7dff3b50e265
Roxanne Desmeules <roxanne@smartreno.com>
2018-01-11 17:15:48 -0500

    display list of all posts, only straight information

--- backend/src/app/admin/post/list/list.component.ts
+++ backend/src/app/admin/post/list/list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Post } from "@core/data/posts/post.model";
+import { PostService } from "@core/data/posts/post.service";
+import { ToasterService } from "angular2-toaster";
 
 @Component({
-	selector    : "app-list",
+	selector    : "ngx-post-list",
 	templateUrl : "./list.component.html",
 	styleUrls   : [ "./list.component.scss" ],
 })
 export class ListComponent implements OnInit {
 
-	constructor () { }
+	public list: Post[];
+
+	constructor ( private _route: ActivatedRoute,
+				  private toastService: ToasterService,
+				  private service: PostService ) { }
 
 	ngOnInit () {
+		this.list = this._route.snapshot.data[ "list" ];
 	}
 
 }
