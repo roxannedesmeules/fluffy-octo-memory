7bfc40f7cbbed9666e836f6561727321f86e45ed
Roxanne Desmeules <roxanne@smartreno.com>
2018-01-27 20:27:28 -0500

    commented pagination model methods and property  completed pagination widget  properly reload category list when pagination changes

--- backend/src/app/@shared/widgets/pagination/pagination.model.ts
+++ backend/src/app/@shared/widgets/pagination/pagination.model.ts
@@ -1,6 +1,16 @@
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
+import { HttpHeaders } from "@angular/common/http";
 
+/**
+ * @class Pagination
+ * Manages the information related to paging a list. Currently only method supported is by HttpHeaders.
+ *
+ * @property {number} currentPage current page number - 0 based
+ * @property {number} pageCount number of pages for the current list
+ * @property {number} perPage number of items displayed per page
+ * @property {number} totalCount total number of items in the non-paged list
+ */
 export class Pagination {
 	private _subject = new Subject<any>();
 
@@ -11,29 +21,51 @@ export class Pagination {
 
 	constructor () {}
 
+	/**
+	 * Return the model's information as a plain object.
+	 *
+	 * @returns {object}
+	 */
 	public getData (): object {
 		return {
-			currentPage : this.currentPage,
-			pageCount   : this.pageCount,
-			perPage     : this.perPage,
-			totalCount  : this.totalCount,
+			currentPage: this.currentPage,
+			pageCount  : this.pageCount,
+			perPage    : this.perPage,
+			totalCount : this.totalCount,
 		};
 	}
 
+	/**
+	 * Returns the private _subject property as an Observable so any changes made to the model can easily be
+	 * communicated to the component with the list.
+	 *
+	 * @returns {Observable<any>}
+	 */
 	public getService (): Observable<any> {
 		return this._subject.asObservable();
 	}
 
-	public setPagination ( headers ) {
-		this.totalCount  = Number(headers.get("X-Pagination-Total-Count"));
-		this.pageCount   = Number(headers.get("X-Pagination-Page-Count"));
-		this.currentPage = Number(headers.get("X-Pagination-Current-Page"));
-		this.perPage     = Number(headers.get("X-Pagination-Per-Page"));
+	/**
+	 * Defines the model information from the header object passed in parameter.
+	 *
+	 * @param {HttpHeaders} headers
+	 */
+	public setPagination ( headers: HttpHeaders ) {
+		this.totalCount  = Number( headers.get( "X-Pagination-Total-Count" ) );
+		this.pageCount   = Number( headers.get( "X-Pagination-Page-Count" ) );
+		this.currentPage = Number( headers.get( "X-Pagination-Current-Page" ) );
+		this.perPage     = Number( headers.get( "X-Pagination-Per-Page" ) );
 	}
 
+	/**
+	 * Called to update the current page number. 1 is removed from the number since the pagination for any backend
+	 * purpose is 0 based while the numbers displayed start at 1.
+	 *
+	 * @param {number} pageNumber
+	 */
 	updateCurrentPage ( pageNumber: number ) {
 		this.currentPage = (pageNumber - 1);
 
-		this._subject.next(this.getData());
+		this._subject.next( this.getData() );
 	}
 }
--- backend/src/app/admin/category/list/list.component.ts
+++ backend/src/app/admin/category/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs/Subscription";
 import { ToasterService } from "angular2-toaster";
@@ -9,71 +9,91 @@ import { ErrorResponse } from "@core/data/error-response.model";
 import { CategoryService } from "@core/data/categories/category.service";
 import { Pagination } from "@shared/widgets/pagination/pagination.model";
 
-@Component({
-	selector    : "ngx-category-list",
-	templateUrl : "./list.component.html",
-	styleUrls   : [ "./list.component.scss" ],
-})
-export class ListComponent implements OnInit {
+@Component( {
+	selector: "ngx-category-list",
+	templateUrl: "./list.component.html",
+	styleUrls: [ "./list.component.scss" ],
+} )
+export class ListComponent implements OnInit, OnDestroy {
 	private _subscriptions: Subscription[] = [];
 
 	public categories: Category[];
 	public languages: Lang[];
-	public statuses: any[] = [ { id : -1, name : "All" }, { id : 0, name : "Inactive" }, { id : 1, name : "Active" }, ];
+	public statuses: any[] = [ { id: -1, name: "All" }, { id: 0, name: "Inactive" }, { id: 1, name: "Active" } ];
 
 	public pagination: Pagination = new Pagination();
 
 	constructor ( private _route: ActivatedRoute,
-				  private toastService: ToasterService,
-				  private service: CategoryService ) { }
+					private toastService: ToasterService,
+					private service: CategoryService ) { }
 
 	ngOnInit () {
-		this.pagination.setPagination(this._route.snapshot.data[ "list" ].headers);
+		this.pagination.setPagination( this._route.snapshot.data[ "list" ].headers );
 
 		this.categories = this._route.snapshot.data[ "list" ].body;
-		this.languages  = this._route.snapshot.data[ "languages" ];
-
-		console.log(this.pagination);
+		this.languages = this._route.snapshot.data[ "languages" ];
 
 		//  update list on pagination change
-		this._subscriptions[ "pagination" ] = this.pagination.getService().subscribe(this.updatePagination);
+		this._subscriptions[ "pagination" ] = this.pagination
+													.getService()
+													.subscribe( (data) => { this.updatePagination(data); });
 	}
 
+	ngOnDestroy () {
+		this._subscriptions[ "pagination" ].removeAll();
+	}
+
+	/**
+	 * Call service to make API request and delete the record associated to the category ID passed in parameter.
+	 *
+	 * @param categoryId
+	 */
 	deleteOne ( categoryId ) {
 		this.service
-				.delete(categoryId)
-				.then(() => {
-					this.updateList();
-				})
-				.catch(( error: ErrorResponse ) => {
-					this.toastService.popAsync("error", "Ouch", error.message);
-				});
+			.delete( categoryId )
+			.then( () => {
+				this.updateList();
+			} )
+			.catch( ( error: ErrorResponse ) => {
+				this.toastService.popAsync( "error", "Ouch", error.message );
+			} );
 	}
 
+	/**
+	 *
+	 * @param attr
+	 * @param value
+	 */
 	filter ( attr, value ) {
-		this.service.filters.set(attr, value);
+		this.service.filters.set( attr, value );
 
 		this.updateList();
 	}
 
+	/**
+	 * This method is called is time a change is made to the pagination object (current page, page size, ...). The
+	 * service filters will be updated accordingly, then the list will be updated.
+	 *
+	 * @param data
+	 */
 	updatePagination ( data ) {
-		this.service.filters.setPagination(this.pagination);
+		this.service.filters.setPagination( this.pagination );
 
 		this.updateList();
 	}
 
 	updateList () {
 		this.service
-				.findAll()
-				.then(( result: any ) => {
-					//  update pagination information
-					this.pagination.setPagination(result.headers);
+			.findAll()
+			.then( ( result: any ) => {
+				//  update pagination information
+				this.pagination.setPagination( result.headers );
 
-					//  set categories list
-					this.categories = this.service.mapListToModelList(result.body);
-				})
-				.catch(( error: ErrorResponse ) => {
-					this.toastService.popAsync("error", "Ouch", error.message);
-				});
+				//  set categories list
+				this.categories = this.service.mapListToModelList( result.body );
+			} )
+			.catch( ( error: ErrorResponse ) => {
+				this.toastService.popAsync( "error", "Ouch", error.message );
+			} );
 	}
 }
