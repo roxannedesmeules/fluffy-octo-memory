903a7bffe1ddd0250e52d8eeb651df03512b4fd8
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2018-02-11 21:25:27 -0500

    updated user profile model :   - transform birthday to datepicker object   - get user initials  create profile page :   - picture placeholder with user's initials   - display user's name   - create simple form for name and birthday

--- backend/src/app/@core/data/users/user-profile.model.ts
+++ backend/src/app/@core/data/users/user-profile.model.ts
@@ -12,4 +12,21 @@ export class UserProfile {
 			this.birthday  = model.birthday;
 		}
 	}
+
+	/**
+	 * Transform the birthday date to an object to be use by a datepicker.
+	 *
+	 * @return {object}
+	 */
+	public birthdayToDatepicker (): object {
+		const date = new Date(this.birthday);
+
+		return { year : date.getFullYear(), month : date.getMonth() + 1, day : date.getDate() + 1 };
+	}
+
+	public  getInitials (): string {
+		const names = this.fullname.split(" ");
+
+		return names.map(n => n.charAt(0)).splice(0, 2).join("").toUpperCase();
+	}
 }
--- backend/src/app/admin/user/profile/profile.component.ts
+++ backend/src/app/admin/user/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+
+import { User } from "@core/data/users/user.model";
+import { UserService } from "@core/data/users/user.service";
 
 @Component({
 	selector    : "ngx-user-profile",
@@ -7,10 +12,29 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ProfileComponent implements OnInit {
 
-	constructor () {
-	}
+	public form: FormGroup;
+	public user: User;
+
+	constructor ( private _route: ActivatedRoute,
+				  private _builder: FormBuilder,
+				  private service: UserService ) {}
 
 	ngOnInit () {
+		this.user = this._route.snapshot.data[ "user" ];
+
+		this._createForm();
 	}
 
+	/**
+	 * Create the Form group to be used in the profile page.
+	 *
+	 * @private
+	 */
+	private _createForm () {
+		this.form = this._builder.group({
+			firstname : this._builder.control(this.user.profile.firstname, [ Validators.required ]),
+			lastname  : this._builder.control(this.user.profile.lastname, [ Validators.required ]),
+			birthday  : this._builder.control(this.user.profile.birthdayToDatepicker(), [ Validators.required ]),
+		});
+	}
 }
