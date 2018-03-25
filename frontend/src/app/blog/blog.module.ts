89534d1cab31f3bcaa59a93aed5b5bc6912be205
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2018-03-24 21:00:16 -0400

    created a blog footer component  moved tags component to tutorial folder

--- frontend/src/app/blog/blog.module.ts
+++ frontend/src/app/blog/blog.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { BlogRoutingModule } from "./blog-routing.module";
-import { TagsComponent } from "./tags/tags.component";
+import { TagsComponent } from "./tutorial/tags/tags.component";
 import { TutorialComponent } from "./tutorial/tutorial.component";
+import { FooterComponent } from './tutorial/footer/footer.component';
 
 const COMPONENTS = [
+	FooterComponent,
 	TagsComponent,
 	TutorialComponent,
 ];
