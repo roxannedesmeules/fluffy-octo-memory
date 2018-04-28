19333b5097e6936c523398df89b3fc1c19765dd4
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2018-04-28 12:18:58 -0400

    implement logout functionality  replace index.html link for /admin/dashboard on logo

--- backend/src/app/@theme/components/header/header.component.ts
+++ backend/src/app/@theme/components/header/header.component.ts
@@ -1,19 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "@core/data/users";
 
 @Component({
 	selector    : "app-layout-header",
 	templateUrl : "./header.component.html",
 	styleUrls   : [ "./header.component.scss" ],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
 	@Input() isShrinked:boolean = false;
 	@Output() toggleSidemenu: EventEmitter<boolean> = new EventEmitter();
 
-	constructor () {
-	}
+	constructor ( private _router: Router, private service: AuthService ) {}
 
-	ngOnInit () {
+	public logout () {
+		this.service.logout()
+			.subscribe(
+				() => { this._router.navigate([ "/auth/login" ]); },
+				() => { this._router.navigate([ "/auth/login" ]); }
+			);
 	}
-
 }
