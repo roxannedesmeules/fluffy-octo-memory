0d1b9155a2da2341715ac5ef78894578954c4c7d
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2018-05-06 16:03:21 -0400

    fixed slug pipe :   - remove trailing space   - remove all punctuation   - remove any extra spaces

--- backend/src/app/@shared/pipes/string/slug.pipe.ts
+++ backend/src/app/@shared/pipes/string/slug.pipe.ts
@@ -6,14 +6,22 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class SlugPipe implements PipeTransform {
 
 	transform ( value: string ): string {
-		//  replace all spaces by a dash
-		let slug = value.replace(/ /g, "-");
+		let slug = value;
 
-		//  remove all apostrophe
-		slug = slug.replace(/'/g, "");
+		//  remove all punctuations
+		slug = slug.replace(/[.,'\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
 		//  replace all accents
 		slug = slug.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+		//  remove all extra spaces
+		slug = slug.replace(/\s{2,}/g," ");
+
+		//  remove space if last character
+		slug = slug.replace(/\s+$/, '');
+
+		//  replace all spaces by a dash
+		slug = slug.replace(/ /g, "-");
 
 		//  return a valid slug
 		return slug.toLowerCase();
